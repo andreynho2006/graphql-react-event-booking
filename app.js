76b2ad484a9a3f508d2b827c2f2a5468e8911da4
andreynho2006 <andreynho2006@gmail.com>
2020-03-10 22:24:57 +0200

    make model for Event and save data on mongo on creating an event

--- app.js
+++ app.js
@@ -1,10 +1,102 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
+
+//const events = [];
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world');
-});
-app.listen(3000);
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput) : Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }       
+    `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventInput.title,
+        //   description: args.eventInput.description,
+        //   price: +args.eventInput.price,
+        //   date: args.eventInput.date
+        // };
+        //events.push(event);
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1-xeeqf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+  )
+  .then(() => {
+    console.log('Database connection succesufull');
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
