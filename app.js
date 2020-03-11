865d5d2e6d5bd602d204a9c72038f09cab60cb88
andreynho2006 <andreynho2006@gmail.com>
2020-03-11 09:28:06 +0200

    create user with a bcrypt password, save to mongodb, check if user already exists and retrieve users

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -24,6 +25,12 @@ app.use(
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
@@ -31,12 +38,19 @@ app.use(
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
+            users: [User!]!
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput) : Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -81,7 +95,39 @@ app.use(
             console.log(err);
             throw err;
           });
-        return event;
+      },
+      users: () => {
+        return User.find()
+          .then(users => {
+            return users.map(user => {
+              return { ...user._doc, password: null };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email }).then(user => {
+          if (user) {
+            throw new Error('UserAlready exists');
+          }
+          return bcrypt
+            .hash(args.userInput.password, 12)
+            .then(hashPassword => {
+              const user = new User({
+                email: args.userInput.email,
+                password: hashPassword
+              });
+              return user.save();
+            })
+            .then(result => {
+              return { ...result._doc, password: null, _id: result.id };
+            })
+            .catch(err => {
+              throw err;
+            });
+        });
       }
     },
     graphiql: true
