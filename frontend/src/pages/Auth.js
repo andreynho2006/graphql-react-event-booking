2fa8a5f8a4e4670e9507a2a9c35e72340d73db40
andreynho2006 <andreynho2006@gmail.com>
2020-03-12 18:17:55 +0200

    style the buttons

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import './Auth.css';
+import { formatError } from 'graphql';
 
 class AuthPage extends Component {
   render() {
-    return <h1>Auth Page</h1>;
+    return (
+      <form className='auth-form'>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' />
+        </div>
+
+        <div className='form-actions'>
+          <button type='submit'>Submit</button>
+          <button type='button'>Switch to Signup</button>
+        </div>
+      </form>
+    );
   }
 }
 
