548e3fe96a63c30b7b7bd99cc0d8239eca23a83e
andreynho2006 <andreynho2006@gmail.com>
2020-03-12 18:48:40 +0200

    add headers for CORS

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const isAuth = require('./middleware/is-auth');
 //const events = [];
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -30,7 +41,7 @@ mongoose
   )
   .then(() => {
     console.log('Database connection succesufull');
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch(err => {
     console.log(err);
